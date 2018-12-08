248dc0db4505779fb48e3b27fc58dd0901898eb4
mythLabs <amitbisht744@gmail.com>
2018-12-08 11:00:38 +0530

    Added Redux setup

---
+++ src/components/auth/Signup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Signup extends React.Component {
+     render() {
+         return (
+             <form>
+                 <fieldset>
+                     <label>Email</label>
+                 </fieldset>
+                 <fieldset>
+                     <label>Password</label>
+                 </fieldset>
+             </form>         )
+     }
+}
+
+export default Signup;
