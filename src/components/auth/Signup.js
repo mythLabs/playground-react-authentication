fbec5a8fa8b600a9792b1ce1bad98557e4c9722f
mythLabs <amitbisht744@gmail.com>
2018-12-08 15:22:39 +0530

    Handled form submittion

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React from "react";
+import { reduxForm, Field } from "redux-form";
 
 class Signup extends React.Component {
-     render() {
-         return (
-             <form>
-                 <fieldset>
-                     <label>Email</label>
-                 </fieldset>
-                 <fieldset>
-                     <label>Password</label>
-                 </fieldset>
-             </form>         )
-     }
+     onSubmit = (fromProps) => {
+          console.log(fromProps);
+     };
+
+
+  render() {
+     const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit = {handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label>Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <button>Sign up</button>
+      </form>
+    );
+  }
 }
 
-export default Signup;
+export default reduxForm({ form: "signup" })(Signup);
