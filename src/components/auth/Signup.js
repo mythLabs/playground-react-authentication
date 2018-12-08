96eda35bb8d3df4bfa145b0207bb843d12f7b445
mythLabs <amitbisht744@gmail.com>
2018-12-08 18:08:46 +0530

    Added signup API call/action with thunk

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+import { compose } from "redux";
 
 class Signup extends React.Component {
-     onSubmit = (fromProps) => {
-          console.log(fromProps);
-     };
-
+  onSubmit = formProps => {
+    this.props.signup(formProps,() => {
+      this.props.history.push('/feature');
+    });
+  };
 
   render() {
-     const { handleSubmit } = this.props;
+    const { handleSubmit } = this.props;
 
     return (
-      <form onSubmit = {handleSubmit(this.onSubmit)}>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label>Email</label>
           <Field
@@ -30,10 +34,20 @@ class Signup extends React.Component {
             autoComplete="none"
           />
         </fieldset>
+        <div>
+          {this.props.errorMessage}
+        </div>
         <button>Sign up</button>
       </form>
     );
   }
 }
 
-export default reduxForm({ form: "signup" })(Signup);
+function mapStateToProps(state) {
+  return {errorMessage: state.auth.errorMessage}
+}
+
+export default compose(
+  connect(mapStateToProps,actions),
+  reduxForm({ form: "signup" })
+)(Signup);
